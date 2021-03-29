5a4c181d17780c4f751dcb76e110c21afe56a065
Yash-y-ash <wardhan696@gmail.com>
2021-03-29 23:09:01 +0530

    Added:regular expression for cheking email id

--- src/com/sampleemail/RegularExpressionForEmail.java
+++ src/com/sampleemail/RegularExpressionForEmail.java
@@ -1,10 +1,31 @@
 package com.sampleemail;
 
+import java.util.*;
+import java.util.regex.*;
+
 public class RegularExpressionForEmail {
+	
+	public static boolean email() {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter email: ");
+		String email = sc.next();
+		if (Pattern.matches(
+				"[a-zA-Z0-9][a-zA-Z0-9_.]*@[a-zA-Z0-9]+([.][a-zA-Z]+)+",
+				email)) {
+
+			System.out.println("Email Id valid");
+			return true;
+		} else {
+			System.out.println("Email Id invalid");
+			return email();
+		}
+	}
 
 	public static void main(String[] args) {
 		
-		System.out.println("welcome message");
+		System.out.println("Welcome to user registration");
+		email();
+		
 	}
 
 }
